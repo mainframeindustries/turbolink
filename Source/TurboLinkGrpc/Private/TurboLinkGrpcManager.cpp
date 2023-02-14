8da9667129bcd210efd569ffa30061e6e7c71bd3
ZezhongWang <904192625@qq.com>
2023-02-14 10:46:44 +0200

    Add ConnectOnInit option to config

--- Source/TurboLinkGrpc/Private/TurboLinkGrpcManager.cpp
+++ Source/TurboLinkGrpc/Private/TurboLinkGrpcManager.cpp
@@ -32,6 +32,19 @@ void UTurboLinkGrpcManager::Initialize(FSubsystemCollectionBase& Collection)
 	//Create global completion queue
 	d->CompletionQueue = std::make_unique<grpc::CompletionQueue>();
 	bIsInitialized = true;
+
+	// Connect to all service endpoint if enable ConnectOnInit
+	FTurboLinkGrpcModule* turboLinkModule = FModuleManager::GetModulePtr<FTurboLinkGrpcModule>("TurboLinkGrpc");
+	UTurboLinkGrpcConfig* config = turboLinkModule->GetTurboLinkGrpcConfig();
+	if (config->ConnectOnInit)
+	{
+		for (int32 i = 0; i < seriviceClasses.Num(); i++)
+		{
+			UClass* serviceClass = seriviceClasses[i];
+			UGrpcService* service = MakeService(serviceClass->GetName());
+			service->Connect();
+		}
+	}
 }
 
 void UTurboLinkGrpcManager::Deinitialize()
